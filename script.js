039f407231bebcc95654456aa8a9369cec57bbc9
dimitriantoniou <dma2012@gmail.com>
2020-08-15 09:01:18 -0500

    added constant nav bar; started 'current page' anv bar feature

--- script.js
+++ script.js
@@ -57,3 +57,15 @@ function closeSubmenu(e) {
 
 /* Event listener */
 document.addEventListener("click", closeSubmenu, false);
+
+/*current page*/
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script>
+    $(function(){
+    $('a').each(function() {
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('current');
+        }
+    });
+});
+</script>
