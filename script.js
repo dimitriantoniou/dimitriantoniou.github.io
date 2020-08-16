63ceb9bd64e592602404c0ce869e5559b9dde4f9
dimitriantoniou <dma2012@gmail.com>
2020-08-16 15:13:36 -0500

    installed and linked jquery; deleted old js; deleted old CSS

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-//js for nav bar and menu
 
+/*highlight current page in nav bar*/
+//producing "$ is not defined" error
+jQuery(document).ready(function($){
+    // Get current page URL
+    var url = window.location.href;
 
+    // remove # from URL
+    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
 
-/*current page*/
-<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
-<script>
-    $(function(){
-    $('a').each(function() {
-        if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('current');
+    // remove parameters from URL
+    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+
+    // select file name
+    url = url.substr(url.lastIndexOf("/") + 1);
+
+    // If file name not available
+    if(url == ''){
+        url = 'page.html';
+    }
+
+    // Loop all menu items
+    $('.menu li').each(function(){
+
+        // select href
+        var href = $(this).find('a').attr('href');
+
+        // Check filename
+        if(url == href){
+
+            // Add active class
+            $(this).addClass('active');
         }
     });
 });
-</script>
 
