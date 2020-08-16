728f7687227008a4f6f4612cbbed7d8467469047
dimitriantoniou <dma2012@gmail.com>
2020-08-16 15:57:41 -0500

    created js script to reference common nav and footer pages

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
 /* trying to create a common nav bar file to reference
 $(document).ready(function(){
-    $("#navbar-frame").load("navbar.html");
+    $("#navbar-frame").load("nav.html");
 });
  */
+
+fetch("./footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
+
+fetch("./nav.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("nav").innerHTML = data;
+    });
